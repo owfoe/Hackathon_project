8758136892e6bd1d5080aec9483a738faf9f88b7
owfoe <gvinnik07@gmail.com>
2024-04-13 14:24:31 +0300

    start prog

---
+++ main.py
@@ -0,0 +1,34 @@
+import json
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+import asyncio
+import json
+
+import logging
+import sys
+from os import getenv
+with open('BOT_TOKEN.json') as file:
+    data = json.load(file)
+
+TOKEN = data['key']
+print(TOKEN)
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}")
+
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
